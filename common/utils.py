a86f700d1ebd860b4bc382df90cb0b389ba9a255
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-13 16:35:23 +0000

    Adding tarfile member sanitization to extractall()

--- common/utils.py
+++ common/utils.py
@@ -109,7 +109,26 @@ def unarchive(archive_path: Path, working_folder: Path) -> TaskStatus:
                             module_logger.debug(product_name)
                             break
 
-                    tf.extractall(path=Path(extraction_path, product_name))
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tf, path=Path(extraction_path,product_name))
 
             except tarfile.TarError as e:
                 module_logger.error(
